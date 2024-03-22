37a02f9a135c5c2fd2ea03e6f56a958812878a3d
Norbiros <norbiros@protonmail.com>
2024-03-22 20:22:29 +0100

    chore: Reformat code!

--- gwardia_hub/wsgi.py
+++ gwardia_hub/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gwardia_hub.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gwardia_hub.settings")
 
 application = get_wsgi_application()
