a01c9acf0677132b402ee02b014a3f3a058a12ae
Kubaryt <kubastarzyk6554@gmail.com>
2024-03-25 23:34:08 +0100

    Update based on #16 feedback - deleted not used colors - changed Discord icon hover color to indigo-500 - Trivia

--- static/GwardiaHub/tailwind_custom_styles.js
+++ static/GwardiaHub/tailwind_custom_styles.js
@@ -5,7 +5,6 @@ tailwind.config = {
         extend: {
           colors: {
             main_bg: '#0d1017',
-            border_bg: '#484f58',
             header_text: '#afbac5',
             basic_text: '#afbac5',
           },
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,6 @@ module.exports = {
     extend: {
       colors: {
         main_bg: '#0d1017',
-        border_bg: '#484f58',
         header_text: '#afbac5',
         basic_text: '#afbac5',
       },
