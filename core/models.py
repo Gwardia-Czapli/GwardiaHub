c300bed07004ec33b126be663a81405d1336e30d
Dominik <162495043+tapczanek1321@users.noreply.github.com>
2024-03-25 18:28:21 +0100

    feat: database model for meetings

--- core/models.py
+++ core/models.py
@@ -1,1 +1,11 @@
+from django.db import models
 # Create your models here.
+
+
+class NextMeeting(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    date_of_meeting = models.DateTimeField("Data spotkania")
+
+    def __str__(self):
+        return self.title
