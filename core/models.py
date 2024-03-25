378e8784e6418f3d76f1305cf71fe59d9a9aa074
Dominik <162495043+tapczanek1321@users.noreply.github.com>
2024-03-25 20:56:15 +0100

    feat: changed language of all texts to english

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class NextMeeting(models.Model):
     title = models.CharField(max_length=150)
     description = models.TextField()
-    date_of_meeting = models.DateTimeField("Data spotkania")
+    date_of_meeting = models.DateTimeField("Date of meeting")
 
     def __str__(self):
         return f"{self.title} - {self.description}"
