0a6e6634b9d0b048b0f9eadfff66c9d231e24a8f
Max <maksymilian.idzik@lo31.krakow.pl>
2024-04-03 18:14:11 +0200

    feat: Created gwardia app :)

---
+++ gwardia/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from .models import NextMeeting
+
+admin.site.register(NextMeeting)
---
+++ gwardia/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GwardiaConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "gwardia"
