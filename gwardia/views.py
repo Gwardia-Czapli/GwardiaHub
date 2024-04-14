fd85a5c0f8f1c5e9d16a87aaca1b187b62d7888d
Kubaryt <88268063+Kubaryt@users.noreply.github.com>
2024-04-14 21:00:03 +0200

    feat: Upcoming meetings list on gwardia/index (#23) (#34)

--- gwardia/views.py
+++ gwardia/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import NextMeeting
 
 
 def index(request):
-    return render(request, "gwardia/index.html")
+    upcoming_meetings = NextMeeting.objects.filter(date_of_meeting__gte=timezone.now()).order_by("date_of_meeting")
+    context = {"upcoming_five_meetings": upcoming_meetings[:5]}
+    return render(request, "gwardia/index.html", context)
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,6 @@ module.exports = {
   content: [],
   theme: {
     extend: {
-      colors: {
-        main_bg: '#0d1017',
-        header_text: '#afbac5',
-        basic_text: '#afbac5',
-      },
       borderWidth: {
         '12': '12px',
       },
