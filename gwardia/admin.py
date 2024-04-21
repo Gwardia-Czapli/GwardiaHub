0283ecfb8fcef11a1c238acca30611da03f73b08
amen <uneventable3106@gmail.com>
2024-04-21 16:37:51 +0200

    fix #32

--- gwardia/admin.py
+++ gwardia/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from .models import NextMeeting
+from .models import Meeting
 
-admin.site.register(NextMeeting)
+admin.site.register(Meeting)
--- gwardia/views.py
+++ gwardia/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.utils import timezone
 
-from .models import NextMeeting
+from .models import Meeting
 
 
 def index(request):
-    upcoming_meetings = NextMeeting.objects.filter(date_of_meeting__gte=timezone.now()).order_by("date_of_meeting")
+    upcoming_meetings = Meeting.objects.filter(date_of_meeting__gte=timezone.now()).order_by("date_of_meeting")
     context = {"upcoming_five_meetings": upcoming_meetings[:5]}
     return render(request, "gwardia/index.html", context)
