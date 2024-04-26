c9846f15552efcaf3eec6b7fdf0e2165d0a88c95
goteusz-maszyk <mateusz.goszyk@gmail.com>
2024-04-26 21:08:27 +0200

    fix: cleanup panel code

--- gwardia/views.py
+++ gwardia/views.py
@@ -3,6 +3,43 @@ from django.utils import timezone
 
 from .models import Meeting
 
+SIDEBAR_LINKS = {
+    "Gwardia": [
+        {
+            "name": "Spotkania",
+            "url": "/gwardia",
+            "icon": "calendar-days",
+        },
+        {
+            "name": "Ankiety",
+            "url": "#",
+            "icon": "square-poll-horizontal",
+        },
+        {
+            "name": "Prezentacje",
+            "url": "#",
+            "icon": "file-powerpoint",
+        },
+    ],
+    "Klasowe": [
+        {
+            "name": "Obiady",
+            "url": "#",
+            "icon": "bowl-food",
+        },
+        {
+            "name": "Sprawdziany",
+            "url": "#",
+            "icon": "newspaper",
+        },
+        {
+            "name": "Zadania",
+            "url": "#",
+            "icon": "paste",
+        },
+    ],
+}
+
 
 def index(request):
     upcoming_meetings = Meeting.objects.filter(date__gte=timezone.now()).order_by(
@@ -14,5 +51,6 @@ def index(request):
     context = {
         "upcoming_meetings": upcoming_meetings,
         "archival_meetings": archival_meetings,
+        "sidebar_links": SIDEBAR_LINKS,
     }
     return render(request, "gwardia/index.html", context)
