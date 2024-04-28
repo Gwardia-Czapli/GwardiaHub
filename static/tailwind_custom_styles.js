f2f188540bf54c08cafa6a6abf5dec52b3f5e66f
kubaryt <kubastarzyk6554@gmail.com>
2024-04-28 21:21:49 +0200

    feat: Added animation, also fixed custom styles mess

--- static/tailwind_custom_styles.js
+++ static/tailwind_custom_styles.js
@@ -1,14 +1,23 @@
 // Used for custom styles in templates
 
 tailwind.config = {
-      theme: {
+    theme: {
         extend: {
-          borderWidth: {
-            '12': '12px',
-          },
-          width: {
-              "1/8": "12.5%",
-          }
+            borderWidth: {
+                '12': '12px',
+            },
+            width: {
+                "1/8": "12.5%",
+            },
+            keyframes: {
+                levelBarSlideIn: {
+                    '0%': { transform: 'translateX(-100%)' },
+                    '100%': { transform: 'translateX(0)' },
+                }
+            },
+            animation: {
+                levelBarSlideIn: 'levelBarSlideIn 0.9s ease-in-out'
+            },
         },
       },
 }
