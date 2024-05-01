72b06c665cfc40501621c656d2772240c995805a
WitaLisek <kacper.dabkowski28@gmail.com>
2024-05-01 21:13:49 +0200

    Feat: Design basic login page

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = "core"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login, name="login"),  # Dodano tę linię przez WitaLisa
+    path("login", views.login, name="login"),
 ]
--- core/views.py
+++ core/views.py
@@ -4,5 +4,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, "core/index.html")
 
+
 def login(request):
     return render(request, "core/login.html")
