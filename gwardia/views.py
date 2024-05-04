d60fcb969996787119a4f44b0b1ffe266e851c9e
goteusz-maszyk <45947374+goteusz-maszyk@users.noreply.github.com>
2024-05-04 15:36:10 +0200

    Design panel for gwardia app (#61)
    * feat: add Gwardia panel w/ sidebar
    
    * fix: make sidebar unfocusable on mobile by clicking outside of it
    
    * fix: make external links open in new tab
    
    * fix: underline home page link
    
    * feat: sidebar slide-in animation + external link marking
    
    * cleanup: slight panel code cleanup
    
    * feat: full sidebar animation
    
    * fix+cleanup: add dark overlay on mobile sidebar; misc fixes

--- gwardia/views.py
+++ gwardia/views.py
@@ -3,6 +3,24 @@ from django.utils import timezone
 
 from .models import Meeting
 
+SIDEBAR_LINKS = {
+    "Gwardia": [
+        {"name": "Spotkania", "url": "/gwardia", "icon": "calendar-days"},
+        {"name": "Ankiety", "url": "#", "icon": "square-poll-horizontal"},
+        {
+            "name": "Prezentacje",
+            "url": "https://drive.google.com/drive/folders/13xlbrwUslL-as5f41sypAq84mszobigY?usp=sharing",
+            "icon": "file-powerpoint",
+            "new_tab": True,
+        },
+    ],
+    "Klasowe": [
+        {"name": "Obiady", "url": "#", "icon": "bowl-food"},
+        {"name": "Sprawdziany", "url": "#", "icon": "newspaper"},
+        {"name": "Zadania", "url": "#", "icon": "paste"},
+    ],
+}
+
 
 def index(request):
     upcoming_meetings = Meeting.objects.filter(date__gte=timezone.now()).order_by(
@@ -14,5 +32,6 @@ def index(request):
     context = {
         "upcoming_meetings": upcoming_meetings,
         "archival_meetings": archival_meetings,
+        "sidebar_links": SIDEBAR_LINKS,
     }
     return render(request, "gwardia/index.html", context)
