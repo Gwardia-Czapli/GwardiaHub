66e0e117d03a9a71fdfc2a7174fa83876bdb9b75
hexxcate <amelia.gawlik@lo31.krakow.pl>
2024-05-09 22:10:54 +0200

    feat: upgrading the code, adding context to avoid code duplication and fixing the remaining errors

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.templatetags.static import static
 
 
 def index(request):
@@ -21,15 +22,15 @@ def handling_404(request, exception):
     context = {
         "error_id": "404",
         "description": "Ojej! Wygląda na to, że jesteś teraz na drodze do nikąd :o",
-        "image": "images/404_image.png",
+        "image": static("images/404_image.png"),
     }
-    return render(request, "core/404.html", context)
+    return render(request, "core/error.html", context, status=404)
 
 
 def handling_500(request):
     context = {
         "error_id": "500",
         "description": "Ojej, coś jest nie tak z naszym serwerem :(",
-        "image": "images/500_image.png",
+        "image": static("images/500_image.png"),
     }
-    return render(request, "core/500.html", context)
+    return render(request, "core/error.html", context, status=500)
