312e8c657246b8330650c0fd5eb75faa4f286881
Dominik <162495043+tapczanek1321@users.noreply.github.com>
2024-05-11 16:50:17 +0200

    feat: Added meeting duration to meeting model

--- gwardia/models.py
+++ gwardia/models.py
@@ -6,6 +6,7 @@ class Meeting(models.Model):
     title = models.CharField(max_length=150)
     description = models.TextField()
     date = models.DateTimeField("Date of meeting")
+    duration = models.CharField(max_length=30, default="0oO")
 
     def is_archival(self):
         return self.date < timezone.now()
