f72091661280a723bee1b805bb90f2fc01034388
Kubaryt <88268063+Kubaryt@users.noreply.github.com>
2024-05-18 19:08:25 +0200

    Feat: Created `genshin` app (#80)

--- gwardia_hub/settings.py
+++ gwardia_hub/settings.py
@@ -49,10 +49,11 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
+    "whitenoise.runserver_nostatic",
     "core",
     "fontawesomefree",
+    "genshin",
     "gwardia",
 ]
 
--- gwardia_hub/urls.py
+++ gwardia_hub/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("core.urls")),
+    path("genshin", include("genshin.urls")),
     path("panel/", include("gwardia.urls")),
     path("admin/", admin.site.urls),
 ]
